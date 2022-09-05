66b5a79d646cff06a6408ae82635583768ca61d6
Dhananjay Jain <jndhananjay2@gmail.com>
2022-09-05 12:43:47 +0530

    Second Commit

--- src/main/java/com/stacksimplify/restservices/SpringbootBuildingblocksPracticeApplication.java
+++ src/main/java/com/stacksimplify/restservices/SpringbootBuildingblocksPracticeApplication.java
@@ -2,7 +2,7 @@ package com.stacksimplify.restservices;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-//
+//Spring boot building blocks
 @SpringBootApplication
 public class SpringbootBuildingblocksPracticeApplication {
 
